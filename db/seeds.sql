ec2acaeef4df7400aebc70439f397c0bb3948363
rich1081 <rich1081@yahoo.com>
2018-01-07 07:06:17 -0600

    updated files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chile Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vietnamese Burger', true);
+
