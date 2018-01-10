db603c2e0781d4f4c925a847b8375c9d4f395240
rich1081 <rich1081@yahoo.com>
2018-01-10 17:46:47 -0600

    pushing jaws_db

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 //setting up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+
+connection =  mysql.createConnection({
 	port: 3306, 
 	host: "localhost",
 	user: "root",
 	password: "MyNewPass",
 	database:  "burger_db"
-});
+ });
+};
 
 //Make connection.
 
